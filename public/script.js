f36cdcddbdbac5cf655e9d08f92e056a70f80ba1
AkshatSachan <a.sachanaaa@gmail.com>
2020-12-12 23:44:15 +0530

    Adding design and formatting... to the home page of the application

--- public/script.js
+++ public/script.js
@@ -1,14 +1,45 @@
 function setup(){
+    
+    
     noCanvas();
     const video = createCapture(VIDEO);
     video.size(320,240);
-
+    video.position(100,125);
+    
     if('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition( async (position) => {
     
         const lat = await  position.coords.latitude;
         const lon = await position.coords.longitude;
         document.getElementById('btn').addEventListener('click',async ()=>{
+
+            const ans = document.createElement("div");
+            
+            ans.setAttribute("class","alert alert-success");
+
+            const first = document.createElement("span");
+            first.setAttribute("class","spanclass");
+            first.textContent = "Lattitude: ";
+            
+
+            const second = document.createElement("span");
+            second.setAttribute("class","spanclass");
+            second.textContent = "Longitude: ";
+            
+
+            const one = document.createElement("span");
+            one.setAttribute("id","lat");
+            first.appendChild(one);
+
+            const two = document.createElement("span");
+            two.setAttribute("id","lon");
+            second.appendChild(two);
+
+            ans.appendChild(first);
+            ans.appendChild(second);
+
+            document.getElementById('div1').appendChild(ans);
+
             video.loadPixels();
             const image64 = video.canvas.toDataURL();
             //console.log(image64);
