72deb818419c8e1da2326293918dd7ad7116fd31
AkshatSachan <a.sachanaaa@gmail.com>
2020-12-21 00:57:24 +0530

    Making use of IP Address -> Now the data of each user can be retrieved using IP address -> Data of other users stays safe

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ database.loadDatabase();
 //database.insert({name: 'something',grade:'1'});
 //database.insert({name: 'somethiny',grade:'2'});
 //Adding routes
+
 app.get('/api',(request,response)=>{
-    database.find({},(err,data)=>{
+    const current_ip = request.headers['x-forwarded-for'] || 
+    request.connection.remoteAddress || 
+    request.socket.remoteAddress ||
+    (request.connection.socket ? request.connection.socket.remoteAddress : null);
+
+    database.find({ip: current_ip},(err,data)=>{
         if (err)
         {
             response.end();return;
@@ -21,13 +27,18 @@ app.get('/api',(request,response)=>{
 });
 app.post('/api'   // Address where we wish to recieve the post request and which can be used to access the request using fetch in index.html's JS
 , (request,response)=>{     //Callback
-        console.log(request.body);
+        //console.log(request.body);
+        const ip = request.headers['x-forwarded-for'] || 
+                request.connection.remoteAddress || 
+                request.socket.remoteAddress ||
+                (request.connection.socket ? request.connection.socket.remoteAddress : null);
+        //console.log(ip);
         const lat = request.body.lat;
         const lon = request.body.lon;
         const name = request.body.name;
         const image64 = request.body.image64;
         const timestamp = Date.now();
-        const data = {lattitude:lat, longitude: lon,timestamp: timestamp,name: name,image: image64};
+        const data = {lattitude:lat, longitude: lon,timestamp: timestamp,name: name,image: image64,ip: ip};
         response.json({     //used to sent back any form of json data
             status:'success',
             timestamp: timestamp,
